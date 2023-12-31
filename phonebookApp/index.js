d3177a7b0b2aef533276d77cbdb0aa2d073a2a47
Nark <sooryarox2004@gmail.com>
2023-12-31 02:25:03 +0200

    phonebook single app

--- phonebookApp/index.js
+++ phonebookApp/index.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 
 const app = express();
 
+app.use(express.static("dist"));
+
 app.use(express.json());
 
 morgan.token("reqbody", function (req, res) {
@@ -98,7 +100,7 @@ app.post("/api/persons/", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
