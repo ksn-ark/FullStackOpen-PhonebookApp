80a0388055be1a0b8f66e3dffbc145c607ee61bc
Nark <sooryarox2004@gmail.com>
2024-07-22 20:21:16 +0300

    Update App.jsx

--- phonebookFrontend/src/App.jsx
+++ phonebookFrontend/src/App.jsx
@@ -1,58 +1,58 @@
-import Persons from "./components/Persons";
+import Persons from "./components/Persons"
 
-import PersonForm from "./components/PersonForm";
+import PersonForm from "./components/PersonForm"
 
-import Notification from "./components/Notification";
+import Notification from "./components/Notification"
 
-import Filter from "./components/Filter";
+import Filter from "./components/Filter"
 
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
 
-import personService from "./services/persons";
+import personService from "./services/persons"
 
 const App = () => {
   //use state
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([])
 
-  const [shownPersons, setShownPersons] = useState([]);
+  const [shownPersons, setShownPersons] = useState([])
 
-  const [newName, setNewName] = useState("");
+  const [newName, setNewName] = useState("")
 
-  const [newNumber, setNewNumber] = useState("");
+  const [newNumber, setNewNumber] = useState("")
 
-  const [filter, setFilter] = useState("");
+  const [filter, setFilter] = useState("")
 
-  const [notification, setNotification] = useState(null);
+  const [notification, setNotification] = useState(null)
 
-  const [notificationIsError, setNotificationIsError] = useState(false);
+  const [notificationIsError, setNotificationIsError] = useState(false)
 
   //use effect
   useEffect(() => {
     personService
       .getAll()
       .then((response) => {
-        console.log("promise fulfilled");
-        updatePersons(response);
+        console.log("promise fulfilled")
+        updatePersons(response)
       })
       .catch((err) => {
-        console.log("promise unfulfilled", err.code);
-      });
+        console.log("promise unfulfilled", err.code)
+      })
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [])
 
   //functions
   const updatePersons = (newPersons) => {
-    setPersons(newPersons);
-    const newShownPersons = filterPersons(newPersons, filter);
-    setShownPersons(newShownPersons);
-  };
+    setPersons(newPersons)
+    const newShownPersons = filterPersons(newPersons, filter)
+    setShownPersons(newShownPersons)
+  }
 
   const filterPersons = (persons, filterValue) => {
     const newShownPersons = persons.filter((person) =>
       person.name.toLowerCase().includes(filterValue.toLowerCase())
-    );
-    return newShownPersons;
-  };
+    )
+    return newShownPersons
+  }
 
   const updatePerson = (person) => {
     if (
@@ -65,114 +65,114 @@ const App = () => {
         .then((updatedPerson) => {
           const newPersons = persons.map((p) =>
             p.id === updatedPerson.id ? updatedPerson : p
-          ); //copy of persons with updated record
-          updatePersons(newPersons);
-          setNewName("");
-          setNewNumber("");
+          ) //copy of persons with updated record
+          updatePersons(newPersons)
+          setNewName("")
+          setNewNumber("")
           setNotification(
             `Record of ${person.name} is updated to ${person.number}`
-          );
-          setTimeout(() => setNotification(null), 5000);
+          )
+          setTimeout(() => setNotification(null), 5000)
         })
         .catch((error) => {
-          setNotificationIsError(true);
+          setNotificationIsError(true)
           if (error.response.status === 404) {
             setNotification(
               `Record of ${person.name} has already been removed from the server`
-            );
-            const newPersons = persons.filter((p) => p.id !== person.id);
-            updatePersons(newPersons);
+            )
+            const newPersons = persons.filter((p) => p.id !== person.id)
+            updatePersons(newPersons)
           } else if (error.response.status === 400) {
-            setNotification(`${error.response.data.error}`);
+            setNotification(`${error.response.data.error}`)
           } else {
-            console.log(error.response);
+            console.log(error.response)
           }
           setTimeout(() => {
-            setNotification(null);
-            setNotificationIsError(false);
-          }, 5000);
-        });
+            setNotification(null)
+            setNotificationIsError(false)
+          }, 5000)
+        })
     }
-    setNotification(`Record of ${person.name} is unchanged`);
-    setTimeout(() => setNotification(null), 5000);
-  };
+    setNotification(`Record of ${person.name} is unchanged`)
+    setTimeout(() => setNotification(null), 5000)
+  }
 
   //event handlers
   const handleaddPerson = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
-    const dupCheck = persons.find((person) => person.name === newName);
+    const dupCheck = persons.find((person) => person.name === newName)
 
     const newpersonObject = {
       name: newName,
       number: newNumber,
       id: dupCheck === undefined ? persons.length + 1 : dupCheck.id, // id same as original if name duplicate, done to make updation possible with one argument
-    };
+    }
 
     if (dupCheck !== undefined) {
-      return updatePerson(newpersonObject);
+      return updatePerson(newpersonObject)
     }
 
     personService
       .create(newpersonObject)
       .then((newPerson) => {
-        const newPersons = persons.concat(newPerson);
-        updatePersons(newPersons);
-        setNewName("");
-        setNewNumber("");
-        setNotification(`Record of ${newPerson.name} is added`);
-        setTimeout(() => setNotification(null), 5000);
+        const newPersons = persons.concat(newPerson)
+        updatePersons(newPersons)
+        setNewName("")
+        setNewNumber("")
+        setNotification(`Record of ${newPerson.name} is added`)
+        setTimeout(() => setNotification(null), 5000)
       })
       .catch((error) => {
-        setNotificationIsError(true);
-        setNotification(`${error.response.data.error}`);
+        setNotificationIsError(true)
+        setNotification(`${error.response.data.error}`)
         setTimeout(() => {
-          setNotification(null);
-          setNotificationIsError(false);
-        }, 5000);
-      });
-  };
+          setNotification(null)
+          setNotificationIsError(false)
+        }, 5000)
+      })
+  }
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
+    setNewName(event.target.value)
+  }
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
+    setNewNumber(event.target.value)
+  }
 
   const handleFilterChange = (event) => {
-    const newFilter = event.target.value;
-    setFilter(newFilter);
+    const newFilter = event.target.value
+    setFilter(newFilter)
     const newShownPersons = persons.filter((person) =>
       person.name.toLowerCase().includes(newFilter.toLowerCase())
-    );
-    setShownPersons(newShownPersons);
-  };
+    )
+    setShownPersons(newShownPersons)
+  }
 
   const handleDeletePerson = (person) => {
     if (window.confirm(`Delete ${person.name} ?`)) {
       return personService
         .deleteEntry(person)
         .then(() => {
-          const newPersons = persons.filter((p) => p.id !== person.id);
-          updatePersons(newPersons);
-          setNotification(`Record of ${person.name} is deleted`);
-          setTimeout(() => setNotification(null), 5000);
+          const newPersons = persons.filter((p) => p.id !== person.id)
+          updatePersons(newPersons)
+          setNotification(`Record of ${person.name} is deleted`)
+          setTimeout(() => setNotification(null), 5000)
         })
         .catch(() => {
-          const newPersons = persons.filter((p) => p.id !== person.id);
-          updatePersons(newPersons);
-          setNotificationIsError(true);
-          setNotification(`Record of ${person.name} is already deleted`);
+          const newPersons = persons.filter((p) => p.id !== person.id)
+          updatePersons(newPersons)
+          setNotificationIsError(true)
+          setNotification(`Record of ${person.name} is already deleted`)
           setTimeout(() => {
-            setNotificationIsError(false);
-            setNotification(null);
-          }, 5000);
-        });
+            setNotificationIsError(false)
+            setNotification(null)
+          }, 5000)
+        })
     }
-    return console.log(`Delete of ${person.name} cancelled`);
-  };
+    return console.log(`Delete of ${person.name} cancelled`)
+  }
 
   //returned website
   return (
@@ -195,7 +195,7 @@ const App = () => {
       <h3>Numbers</h3>
       <Persons persons={shownPersons} handleDelete={handleDeletePerson} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
